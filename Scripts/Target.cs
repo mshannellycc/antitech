08f5f3fb6e4c458fd72a353dfae422fc2081cd3d
Isaac Davis <msipp27@gmail.com>
2023-04-18 14:26:07 -0400

    did some fixes
    to try and make sure you can click anywhere on the target

--- Scripts/Target.cs
+++ Scripts/Target.cs
@@ -6,11 +6,16 @@ public class Target : MonoBehaviour
 {
     //My EPITHET IS
     public GameObject BARRIER;
+    public Renderer roadRenderer;
+    public Vector3 roadRendererSize;
 
     // Start is called before the first frame update
     void Start()
     {
-    // make the barrier the child object by default here
+        // make the barrier the child object by default here
+        roadRenderer = gameObject.GetComponent<Renderer>();
+        roadRendererSize = roadRenderer.bounds.size;
+       
     }
 
     // Update is called once per frame
@@ -21,7 +26,9 @@ public class Target : MonoBehaviour
 
         mouse = Camera.main.ScreenToWorldPoint(mouse);
 
-        if(mouse.x == transform.position.x && mouse.y == transform.position.y)
+        // this if statement uses the position - the renderer size so that you can click anywhere on the target
+        //i'm doing /2 because the size is probably the size of the whole thing and we want the size from the middle to the edge
+        if(mouse.x > transform.position.x - roadRendererSize.x/2 && mouse.x < transform.position.x + roadRendererSize.x/2 && mouse.y < transform.position.y + roadRendererSize.y / 2 && mouse.y > transform.position.y - roadRendererSize.y / 2)
         {
             Destroy(BARRIER);
         }
